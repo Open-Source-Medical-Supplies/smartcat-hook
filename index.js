cda1336fba949c75f50af24d5d649bb4cefaa0c3
Josh <hello@joshmleslie.com>
2020-12-21 10:43:17 -0800

    Pain, lots of pain.

--- index.js
+++ index.js
@@ -1,38 +1,218 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-const logger = require("firebase-functions/lib/logger");
-const { Storage } = require("@google-cloud/storage");
+require("firebase-functions/lib/logger/compat");
+// note to self, stop trying to make the new firebase logger happen. It doesn't report.
 
 // Bucket config
+const { Storage } = require("@google-cloud/storage");
 const projectId = "osms-website";
 const keyFilePath = "./key.json";
 const storage = new Storage({ projectId, keyFilePath });
 const bucket = storage.bucket("opensourcemedicalsupplies.org");
 // end Bucket config
 
-exports.upload = (req, res) => {
-  const { security } = process.env;
-  let message = 'success';
-  let parsed;
+// HTTP / SC config
+const Axios = require("axios");
+const scURL = "https://smartcat.com/api/integration/v1/";
+const { smartcatID, smartcatAPI } = process.env;
+const base64Auth = Buffer.from(`${smartcatID}:${smartcatAPI}`).toString(
+	"base64"
+);
+const axios = Axios.create({
+	baseURL: scURL,
+	timeout: 5000,
+	headers: {
+		Authorization: `Basic ${base64Auth}`,
+		Accept: "application/json",
+	},
+});
+// end SC config
 
-  if (
-    !req.body ||
-    !(
-      req.body['security'] === security ||
-      req.header('security') === security
-    )
-  ) {
-    res.status(404).send();
-  }
+/**
+ * Checks for a 'security' value in the body or header.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+const checkSecurity = (req, res) => {
+	const { security } = process.env;
 
-  try {
-    logger.log(req.body);
-  } catch {
-    logger.error('could not parse req.body', req);
-  }
-  res.status(200).send(message);
+	if (
+		!req.body ||
+		!(req.body["security"] === security || req.header("security") === security)
+	) {
+		res.status(404).send();
+		return false;
+	}
+
+	return true;
 };
+
+const AcceptedLangs = new Set([
+	"ar-SA", // Arabic, Saudi Arabia
+	"en-US", // English, United States
+	"zh-CN", // Chinese, Simplified
+	"fr-FR", // French, France
+	"ru-RU", // Russian, Russian Federation
+	"es-ES", // Spanish, Spain
+	"pt-BR", // Portugese, Brazil
+]);
+
+/**
+ * @param {string} sample
+ * @returns {string|false}
+ */
+const getLang = (sample) => {
+	if (!sample) {
+		return false;
+	} else if (AcceptedLangs.has(sample)) {
+		return sample;
+	}
+
+	let target = false;
+	AcceptedLangs.forEach((lang) => {
+		if (lang.includes(sample)) {
+			target = lang;
+		}
+	});
+	return target;
+};
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.upload = async (req, res) => {
+	// Zaps only bind to a specific project(?), so only need to check for security key
+	if (!checkSecurity(req, res)) {
+		return;
+	}
+
+	if (!req.body.projectName.includes("Design Library")) {
+		res.status(404).send();
+		return;
+	}
+
+	console.log("incoming request");
+	console.log(req.body);
+
+	const projectURL = "project/" + req.body.projectID; // attr named in the Zap
+	await axios.get(projectURL).then(
+		/** @param {AxiosSCProjectResult} result */
+		(result) => {
+			console.log("axios request");
+			console.log(result.data);
+
+			/**
+			 * Request all documentID tasks (convoluted) => gives 'id' === taskID
+			 * then have to get the actual export from another endpoint GET ...document/export/{taskID}
+			 * e.g. curl -X POST --header "Accept: application/json"
+			 * "https://smartcat.com/api/integration/v1/document/export?documentIds=94183338c4fdfd1bb54f1100_10&documentIds=94183338c4fdfd1bb54f1100_10&type=target&stageNumber=1"
+			 */
+
+			// const documentIDs = result.data.documents.map(doc => ({
+			// 	name: doc.name,
+			// 	id: doc.id
+			// }));
+
+			// axios.post('document/export', null, {
+			// 	documentIds: documentIDs
+			// })
+			const { documents } = result.data;
+
+			documents.forEach(({ id, name, targetLanguage }, i) => {
+				const docPostURL = "document/export";
+				const docParams = { params: {
+					type: "target",
+					stageNumber: 1, // translation stage
+					documentIds: id,
+				}};
+				// first get the document taskID...
+				axios
+					.post(docPostURL, null, docParams)
+					.then(
+						(result) => {
+							console.log("SC doc POST");
+							console.log(result);
+							// then get the document
+							const docGetURL = `document/export/${result.id}`;
+							return axios.get(docGetURL);
+						},
+						(e) => console.log(e)
+					)
+					.then((result) => {
+						console.log("SC doc GET");
+						console.log(result);
+						// then upload the file to the gBucket
+						const targetLang =
+							targetLanguage.length < 5
+								? getLang(targetLanguage)
+								: targetLanguage;
+						const fileName = targetLang + "_" + name;
+						console.log('filename')
+						console.log(fileName)
+						// const newFile = bucket.file(fileName);
+						// newFile.save(JSON.stringify(result), (e) =>
+						// 	e ? console.log(e) : res.write(fileName + " uploaded")
+						// );
+
+						if (i === documents.length) {
+							res.status(200).send();
+						}
+					});
+			});
+		},
+		(e) => {
+			console.log(e);
+			res.status(400).send();
+		}
+	);
+};
+
+// Typedefs below
+
+/**
+ * @typedef {Object} SmartcatDocument
+ * @property {string} id
+ * @property {string} name
+ * @property {string} creationDate
+ * @property {string} sourceLanguage
+ * @property {string} documentDisassemblingStatus
+ * @property {string} targetLanguage
+ * @property {string} status
+ * @property {number} wordsCount
+ * @property {string} statusModificationDate
+ * @property {boolean} pretranslateCompleted
+ * @property {Object[]} workflowStages
+ * @property {string} externalId
+ * @property {boolean} placeholdersAreEnabled
+ */
+
+/**
+ * @typedef {Object} SmartcatProject
+ * @property {string} id
+ * @property {string} accountId
+ * @property {string} name
+ * @property {string} description
+ * @property {string} creationDate
+ * @property {string} createdByUserId
+ * @property {string} createdByUserEmail
+ * @property {string} modificationDate
+ * @property {number} sourceLanguageId
+ * @property {string} sourceLanguage
+ * @property {string[]} targetLanguages
+ * @property {string} status
+ * @property {string} statusModificationDate
+ * @property {string} clientId
+ * @property {any[]} vendors
+ * @property {Object[]} workflowStages
+ * @property {SmartcatDocument[]} documents
+ * @property {any[]} specializations
+ * @property {string[]} managers
+ * @property {Object} customFields
+ */
+
+/**
+ * @typedef {Object} AxiosSCProjectResult
+ * @property {SmartcatProject} data
+ */
